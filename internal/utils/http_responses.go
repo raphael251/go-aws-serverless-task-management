9e25788c0aedad720482646ba0cc29a231166cdd
Raphael Passos <raphael251@hotmail.com>
2024-01-29 22:32:35 -0300

    chore: add http responses helper to make http responses easier

---
+++ internal/utils/http_responses.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func HttpResponseCreated() (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+	}, nil
+}
+
+func HttpResponseOK(body interface{}) (*events.APIGatewayProxyResponse, error) {
+	type SuccessResponse struct {
+		Message string `json:"message"`
+	}
+
+	response, err := json.Marshal(&SuccessResponse{Message: "success"})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(response),
+	}, nil
+}
