ec9fe7488b58def0a6ccea55dca37de5293a6897
Raphael Passos <raphael251@hotmail.com>
2024-01-29 22:48:43 -0300

    chore: move project handlers to a separated function
    this is a first refactoring. Later I will move it to a separated file for better separation of concerns.

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -33,25 +34,32 @@ func Handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse
 	switch req.Path {
 	case "/api/users":
 		return UsersRouter(req, dbClient)
+	case "/api/projects":
+		return ProjectsRouter(req, dbClient)
+	default:
+		return nil, errors.New(ErrUnexpected)
 	}
-
-	switch req.HTTPMethod {
-	case "POST":
-		return handlers.CreateProject(req, dbClient)
-	case "GET":
-		return handlers.FindAllProjects(req)
-	case "PUT":
-		return handlers.UpdateProject(req)
-	case "DELETE":
-		return handlers.DeleteProject(req)
-	}
-	return nil, errors.New(ErrUnexpected)
 }
 
 func UsersRouter(req events.APIGatewayProxyRequest, dbClient *dynamodb.Client) (*events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
-	case "POST":
+	case http.MethodPost:
 		return handlers.CreateUser(req, dbClient)
+	default:
+		return nil, errors.New(ErrUnexpected)
+	}
+}
+
+func ProjectsRouter(req events.APIGatewayProxyRequest, dbClient *dynamodb.Client) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case http.MethodPost:
+		return handlers.CreateProject(req, dbClient)
+	case http.MethodGet:
+		return handlers.FindAllProjects(req)
+	case http.MethodPut:
+		return handlers.UpdateProject(req)
+	case http.MethodDelete:
+		return handlers.DeleteProject(req)
 	default:
 		return nil, errors.New(ErrUnexpected)
 	}
--- internal/handlers/projects.go
+++ internal/handlers/projects.go
@@ -1,10 +1,16 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
+	"github.com/raphael251/go-aws-serverless-task-management/internal/database"
 )
 
 var ErrUnexpected = "unexpected error"
@@ -15,7 +21,44 @@ type CreateProjectInput struct {
 }
 
 func CreateProject(req events.APIGatewayProxyRequest, dbClient *dynamodb.Client) (*events.APIGatewayProxyResponse, error) {
-	return nil, errors.New(ErrUnexpected)
+	var project *CreateProjectInput
+
+	err := json.Unmarshal([]byte(req.Body), &project)
+
+	if err != nil {
+		return nil, err
+	}
+
+	output, err := dbClient.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		Item: map[string]types.AttributeValue{
+			"my-partition-key": &types.AttributeValueMemberS{Value: uuid.New().String()},
+			"title":            &types.AttributeValueMemberS{Value: project.Title},
+			"description":      &types.AttributeValueMemberS{Value: project.Description},
+		},
+		TableName: &database.AppTableName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Successfully inserted an item in dynamoDB", output)
+
+	type SuccessResponse struct {
+		Message string `json:"message"`
+	}
+
+	response, err := json.Marshal(&SuccessResponse{Message: "success"})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(response),
+	}, nil
 }
 
 func FindAllProjects(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
